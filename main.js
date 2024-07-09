2759ad2a0756ad91d1de94a745f26b8a4d23e6b5
ngothanhbinh204 <125552911+ngothanhbinh204@users.noreply.github.com>
2024-07-09 13:20:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,59 @@ menuButton.addEventListener('click', () => {
     menu.classList.toggle('invisible');
 })
 
+// ------------------------------
+
+const prevBanner1 = document.getElementById('prevBanner1');
+const nextBanner1 = document.getElementById('nextBanner1');
+const carousel = document.getElementById('carousel');
+const indicators = document.querySelectorAll('.indicator');
+let offset = 0;
+
+function updateIndicators() {
+    indicators.forEach((indicator, index) => {
+        if (index === offset) {
+            indicator.classList.add('bg-main-color');
+            indicator.classList.remove('bg-gray-300');
+        } else {
+            indicator.classList.add('bg-gray-300');
+            indicator.classList.remove('bg-main-color');
+        }
+    });
+}
+
+prevBanner1.addEventListener('click', () => {
+    if (offset > 0) {
+        offset--;
+    } else {
+        offset = carousel.childElementCount - 1;
+    }
+    carousel.style.transform = `translateX(-${offset * 100}%)`;
+    updateIndicators();
+});
+
+nextBanner1.addEventListener('click', () => {
+    if (offset < carousel.childElementCount - 1) {
+        offset++;
+    } else {
+        offset = 0;
+    }
+    carousel.style.transform = `translateX(-${offset * 100}%)`;
+    updateIndicators();
+});
+
+indicators.forEach((indicator, index) => {
+    indicator.addEventListener('click', () => {
+        offset = index;
+        carousel.style.transform = `translateX(-${offset * 100}%)`;
+        updateIndicators();
+    });
+});
+
+updateIndicators();
+
+
+// ---------------------------------------
+
 const slider = document.getElementById('slider');
 const slides = slider.children;
 const prev = document.getElementById('prev');
@@ -44,3 +97,46 @@ prev.addEventListener('click', () => {
         slider.style.transform = `translateX(-${index * 100}%)`;
     }
 });
+
+
+// ---------------------------------------
+
+const sliderMap = document.getElementById('sliderMap');
+const slidesMap = sliderMap.children;
+const prevMap = document.getElementById('prevMap');
+const nextMap = document.getElementById('nextMap');
+let indexMap = 0;
+const totalSlidesMap = slidesMap.length - 1;
+
+nextMap.addEventListener('click', () => {
+    if (indexMap >= totalSlidesMap - 1) {
+        indexMap = 0;
+        sliderMap.style.transition = 'none';
+        sliderMap.style.transform = `translateX(0)`;
+        setTimeout(() => {
+            sliderMap.style.transition = 'transform 0.5s ease-in-out';
+            indexMap++;
+            sliderMap.style.transform = `translateX(-${indexMap * 100}%)`;
+        }, 20);
+    } else {
+        indexMap++;
+        sliderMap.style.transform = `translateX(-${indexMap * 100}%)`;
+    }
+});
+
+prevMap.addEventListener('click', () => {
+    if (indexMap <= 0) {
+        indexMap = totalSlides - 1;
+        sliderMap.style.transition = 'none';
+        sliderMap.style.transform = `translateX(-${indexMap * 100}%)`;
+        setTimeout(() => {
+            sliderMap.style.transition = 'transform 0.5s ease-in-out';
+            indexMap--;
+            sliderMap.style.transform = `translateX(-${indexMap * 100}%)`;
+        }, 20);
+    } else {
+        indexMap--;
+        sliderMap.style.transform = `translateX(-${indexMap * 100}%)`;
+    }
+});
+
