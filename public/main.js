63783abad8c2b316c7fb1ca977dfd642c4a0a28f
ngothanhbinh204 <125552911+ngothanhbinh204@users.noreply.github.com>
2024-07-10 20:54:09 +0700

    Delete public directory

--- public/main.js
+++
@@ -1,7 +0,0 @@
-//Menu
-const menu = document.getElementById('menu');
-const menuButton = document.getElementById('menu-button');
-
-menuButton.addEventListener('click', () => {
-    menu.classList.toggle('invisible');
-})
