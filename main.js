0aaf0122554a2be9916a1a0cbae46023679f961b
ngothanhbinh204 <125552911+ngothanhbinh204@users.noreply.github.com>
2024-07-31 05:25:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,75 +1,168 @@
-//Menu
-const menu = document.getElementById('menu');
-const menuButton = document.getElementById('menu-button');
-
-menuButton.addEventListener('click', () => {
-    menu.classList.toggle('invisible');
-})
-// ------------------------------
-
-var swiper = new Swiper(".banner-heading", {
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-// ---------------------------------------
-
-var swiperCachDung = new Swiper(".cachdung-slider", {
-    slidesPerView: 4,
-    spaceBetween: 20,
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
     loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+    spaceBetween: 400,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 150,
+        modifier: 6,
+        slideShadows: false,
     },
+    initialSlide: 1, // Chỉ định slide thứ 2 (0-based index)
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    }, breakpoints: {
-        300: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        1024: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-        },
-    }
-});
+        nextEl: ".swiper-button-next", // Selector cho nút next
+        prevEl: ".swiper-button-prev", // Selector cho nút prev
+    },
+    // autoplay: {
 
-var swiperStore = new Swiper(".store-slider", {
-    slidesPerView: 2,
-    spaceBetween: 200,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    // }
+
     breakpoints: {
-        300: {
-            slidesPerView: 1,
-            spaceBetween: 20,
+        100: {
+            spaceBetween: 200,
+        },
+        576: {
+            spaceBetween: 250,
         },
         768: {
-            slidesPerView: 1,
-            spaceBetween: 20,
+            spaceBetween: 300,
+        },
+        992: {
+            spaceBetween: 300,
         },
         1024: {
-            slidesPerView: 2,
-            spaceBetween: 30,
+            spaceBetween: 400,
         },
+
     }
+
 });
 
-// ---------------------------------------
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelector('.nav-links');
+    const menuToggle = document.querySelector('ion-icon[name="menu"]');
+    const menuItems = document.querySelectorAll('.nav-links a');
+    let menuOpen = false;
+
+    function toggleMenu(e) {
+        if (menuOpen) {
+            e.name = 'menu';
+            navLinks.classList.remove('top-[100%]');
+            navLinks.classList.remove('opacity-100');
+        } else {
+            e.name = 'close';
+            navLinks.classList.add('top-[100%]');
+            navLinks.classList.add('opacity-100');
+        }
+        menuOpen = !menuOpen;
+    }
+
+    function handleClickOutside(event) {
+        if (menuOpen && !navLinks.contains(event.target) && event.target !== menuToggle && !menuToggle.contains(event.target)) {
+            navLinks.classList.remove('top-[100%]');
+            navLinks.classList.remove('opacity-100');
+            menuToggle.name = 'menu';
+            menuOpen = false;
+        }
+    }
+
+    function handleMenuItemClick() {
+        if (menuOpen) {
+            navLinks.classList.remove('top-[100%]');
+            navLinks.classList.remove('opacity-100');
+            menuToggle.name = 'menu';
+            menuOpen = false;
+        }
+    }
+
+    menuToggle.addEventListener('click', function (e) {
+        toggleMenu(e.target);
+    });
+
+    document.addEventListener('click', handleClickOutside);
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', handleMenuItemClick);
+    });
+});
+
+
+var swiper2 = new Swiper(".gallery", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next", // Selector cho nút next
+        prevEl: ".swiper-button-prev", // Selector cho nút prev
+    },
+});
+
+var videoPlayer1 = document.getElementById("video_hero");
+var video1 = document.getElementById("video1");
+function stopVideo1() {
+    videoPlayer1.style.display = 'none';
+    video1.pause()
+}
+
+function playVideo1(file) {
+    videoPlayer1.style.display = "block"
+    video1.play();
+
+}
+
+
+
+var videoPlayer2 = document.getElementById("video_gallery");
+var video2 = document.getElementById("video2");
+
+function stopVideo2() {
+    videoPlayer2.style.display = 'none';
+    video2.pause();
+}
+
+function playVideo2(file) {
+    videoPlayer2.style.display = "block"
+    video2.play();
+}
+
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top > offset && top < offset + height) {
+            navLinks.forEach(link => {
+                const targetLink = document.querySelector(`header nav a[href*="${id}"]`);
+                if (targetLink) {
+                    link.classList.remove('active');
+                    targetLink.classList.add('active');
+                }
+            });
+        }
+    });
+};
+
+function toggleBackground(checkbox) {
+    var item = checkbox.closest('.item-FAQ');
+    if (checkbox.checked) {
+        item.classList.add('bg-[#EFFEFF]', 'border-[#1DC8DA]');
+    } else {
+        item.classList.remove('bg-[#EFFEFF]', 'border-[#1DC8DA]');
+    }
+}
+
 
